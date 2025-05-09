d5d567ad8cfeb200945272f805813ca7269a36ab
Daniel Cazzulino <daniel@cazzulino.com>
2025-05-09 17:15:16 -0300

    Add tool-based retrieval tool over the responses API
    Usage: just like the built-in supported extension `responseClient.AsIChatClient()`, but you can pass arbitrary `ResponseTool` that will be configured automatically before invoking the underlying client.

---
+++ src/AI.Tests/RetrievalTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.AI;
+using OpenAI.Responses;
+using static ConfigurationExtensions;
+
+namespace Devlooped.Extensions.AI;
+
+public class RetrievalTests(ITestOutputHelper output)
+{
+    [SecretsTheory("OpenAI:Key")]
+    [InlineData("gpt-4.1-mini", "Qué es la rebeldía en el Código Procesal Civil y Comercial Nacional?")]
+    [InlineData("gpt-4.1-nano", "What's the battery life in an iPhone 15?", true)]
+    public async Task CanRetrieveContent(string model, string question, bool empty = false)
+    {
+        var client = new OpenAI.OpenAIClient(Configuration["OpenAI:Key"]);
+        var store = client.GetVectorStoreClient().CreateVectorStore(true);
+        try
+        {
+            var file = client.GetOpenAIFileClient().UploadFile("Content/LNS0004592.md", OpenAI.Files.FileUploadPurpose.Assistants);
+            try
+            {
+                client.GetVectorStoreClient().AddFileToVectorStore(store.VectorStoreId, file.Value.Id, true);
+
+                var responses = new OpenAIResponseClient(model, Configuration["OpenAI:Key"]);
+
+                var chat = responses.AsIChatClient(
+                        ResponseTool.CreateFileSearchTool([store.VectorStoreId]))
+                    .AsBuilder()
+                    .UseLogging(output.AsLoggerFactory())
+                    .Build();
+
+                var response = await chat.GetResponseAsync(
+                    [
+                        new ChatMessage(ChatRole.System, "Use file search tool to respond, exclusively. If no content was found, respond just with the string 'N/A' and nothing else."),
+                        new ChatMessage(ChatRole.User, question),
+                    ]);
+
+                output.WriteLine(response.Text);
+                if (empty)
+                    Assert.Equal("N/A", response.Text);
+                else
+                    Assert.True(response.Text.Length > 50);
+            }
+            finally
+            {
+                client.GetOpenAIFileClient().DeleteFile(file.Value.Id);
+            }
+        }
+        finally
+        {
+            client.GetVectorStoreClient().DeleteVectorStore(store.VectorStoreId);
+        }
+    }
+}
---
+++ src/AI/OpenAI/OpenAIResponseClientExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.ClientModel;
+using OpenAI.Responses;
+
+namespace Microsoft.Extensions.AI;
+
+/// <summary>
+/// Provides extension methods for working with OpenAI.Responses.OpenAIResponseClient.
+/// </summary>
+public static class OpenAIResponseClientExtensions
+{
+    /// <summary>Gets an <see cref="IChatClient"/> for use with this <see cref="OpenAIResponseClient"/> that supports 
+    /// additional OpenAI-specific tools.</summary>
+    /// <param name="responseClient">The client.</param>
+    /// <param name="tools">Additional tools to configure in the client before invocation.</param>
+    /// <returns>An <see cref="IChatClient"/> that can be used to converse via the <see cref="OpenAIResponseClient"/>.</returns>
+    public static IChatClient AsIChatClient(this OpenAIResponseClient responseClient, params ResponseTool[] tools)
+        => tools.Length == 0 ? OpenAIClientExtensions.AsIChatClient(responseClient) : new ToolsReponseClient(responseClient, tools).AsIChatClient();
+
+    class ToolsReponseClient(OpenAIResponseClient inner, ResponseTool[] tools) : OpenAIResponseClient
+    {
+        public override Task<ClientResult<OpenAIResponse>> CreateResponseAsync(IEnumerable<ResponseItem> inputItems, ResponseCreationOptions options = null, CancellationToken cancellationToken = default)
+            => inner.CreateResponseAsync(inputItems, AddTools(options), cancellationToken);
+
+        public override AsyncCollectionResult<StreamingResponseUpdate> CreateResponseStreamingAsync(IEnumerable<ResponseItem> inputItems, ResponseCreationOptions options = null, CancellationToken cancellationToken = default)
+            => inner.CreateResponseStreamingAsync(inputItems, AddTools(options), cancellationToken);
+
+        ResponseCreationOptions AddTools(ResponseCreationOptions options)
+        {
+            foreach (var tool in tools)
+                options.Tools.Add(tool);
+
+            return options;
+        }
+    }
+}
