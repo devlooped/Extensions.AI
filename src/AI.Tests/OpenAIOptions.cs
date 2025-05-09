e474f204b93cfd90c211cf17439d5f455e65b4d2
Daniel Cazzulino <daniel@cazzulino.com>
2025-05-09 17:13:38 -0300

    Add common helpers to manage configuration and logging

---
+++ src/AI.Tests/OpenAIOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devlooped.Extensions.AI;
+
+record OpenAIOptions(string Key, string[] Vectors)
+{
+    public static OpenAIOptions Empty { get; } = new();
+
+    public OpenAIOptions() : this("", []) { }
+}
