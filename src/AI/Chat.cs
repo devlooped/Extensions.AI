bb93932ada074a34555a0aa49d51399584f203e0
Daniel Cazzulino <daniel@cazzulino.com>
2025-06-30 18:19:10 -0300

    Add more intuitive API for creating chat messages
    Allows using collection initializer syntax for multiple messages, with a more python-esque style:
    
    ```csharp
    var messages = new Chat()
    {
        { "system", "You are a highly intelligent AI assistant." },
        { "user", "What is 101*3?" },
    };
    ```

---
+++ src/AI/Chat.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using Microsoft.Extensions.AI;
+
+namespace Devlooped.Extensions.AI;
+
+/// <summary>
+/// Collection of <see cref="ChatMessage"/> for more convenient usage 
+/// in fluent construction of chat messages.
+/// </summary>
+public class Chat : IEnumerable<ChatMessage>
+{
+    readonly List<ChatMessage> messages = [];
+
+    /// <summary>
+    /// Adds a message to the list of chat messages. 
+    /// For use with collection initializer syntax.
+    /// </summary>
+    public void Add(ChatMessage message) => messages.Add(message);
+
+    /// <summary>
+    /// Adds a message to the list of chat messages.
+    /// </summary>
+    /// <param name="role">The message role</param>
+    /// <param name="message">The message text</param>
+    /// <remarks>
+    /// Example usage:
+    /// <code>
+    /// var messages = new Chat()
+    /// {
+    ///     { "system", "You are a highly intelligent AI assistant." },
+    ///     { "user", "What is 101*3?" },
+    /// };
+    /// </code>
+    /// </remarks>
+    public void Add(string role, string message)
+        => messages.Add(new ChatMessage(role.ToLowerInvariant() switch
+        {
+            "system" => ChatRole.System,
+            "assistant" => ChatRole.Assistant,
+            "user" => ChatRole.User,
+            _ => new ChatRole(role)
+        }, message));
+
+    IEnumerator<ChatMessage> IEnumerable<ChatMessage>.GetEnumerator() => messages.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => messages.GetEnumerator();
+}
---
+++ src/AI/ChatExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using Microsoft.Extensions.AI;
+
+namespace Devlooped.Extensions.AI;
+
+/// <summary>
+/// Provides usability overloads for the <see cref="IChatClient"/> interface.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class ChatExtensions
+{
+    extension(IChatClient client)
+    {
+        /// <summary>
+        /// Allows passing a <see cref="Chat"/> instance to the chat client
+        /// </summary>
+        /// <param name="chat">The chat messages in a single object.</param>
+        /// <param name="options">The optional chat options.</param>
+        /// <param name="cancellation">Optional cancellation token.</param>
+        public Task<ChatResponse> GetResponseAsync(Chat chat, ChatOptions? options = null, CancellationToken cancellation = default)
+            => client.GetResponseAsync((IEnumerable<ChatMessage>)chat, options, cancellation);
+    }
+}
