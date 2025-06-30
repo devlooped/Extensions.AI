cdab0286dc2ab574a14c2df12a8830cd87b8e849
Daniel Cazzulino <daniel@cazzulino.com>
2025-06-30 19:17:27 -0300

    Add opinionated lib for (mostly?) single-file agents
    Weaving (you know, the actor behind the agent? ;-)) provides an opinionated library and default set of package dependencies, default imports and helpers to make creating "dotnet run" file-based AI agents a breeze.
    
    The sample showcases how trivial the combination of M.E.AI, Devlooped.Extension.AI and Weaving make the authoring of this single file AI agents.

---
+++ src/Weaving/ConsoleInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Weaving;
+
+class ConsoleInitializer
+{
+#pragma warning disable CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+    [ModuleInitializer]
+#pragma warning restore CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+    public static void Init()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            Console.InputEncoding = Console.OutputEncoding = Encoding.UTF8;
+    }
+}
