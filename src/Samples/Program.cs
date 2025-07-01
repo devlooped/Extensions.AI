75072715736a35703fac4d37b6c3deffc1a5eb96
Daniel Cazzulino <daniel@cazzulino.com>
2025-07-01 00:59:33 -0300

    Add opinionated lib for (mostly?) single-file agents (#42)
    * Add opinionated lib for (mostly?) single-file agents
    
    Weaving (you know, the actor behind the agent? ;-)) provides an opinionated library and default set of package dependencies, default imports and helpers to make creating "dotnet run" file-based AI agents a breeze.
    
    The sample showcases how trivial the combination of M.E.AI, Devlooped.Extension.AI and Weaving make the authoring of this single file AI agents.

---
+++ src/Samples/Program.cs
@@ -0,0 +1,20 @@
+﻿// Sample X.AI client usage with .NET
+var messages = new Chat()
+{
+    { "system", "You are a highly intelligent AI assistant." },
+    { "user", "What is 101*3?" },
+};
+
+var grok = new GrokClient(Env.Get("XAI_API_KEY")!);
+
+var options = new GrokChatOptions
+{
+    ModelId = "grok-3-mini", // or "grok-3-mini-fast"
+    Temperature = 0.7f,
+    ReasoningEffort = ReasoningEffort.High, // or GrokReasoningEffort.Low
+    Search = GrokSearch.Auto, // or GrokSearch.On or GrokSearch.Off
+};
+
+var response = await grok.GetResponseAsync(messages, options);
+
+AnsiConsole.MarkupLine($":robot: {response.Text}");
---
+++ src/Weaving/readme.md
@@ -0,0 +1,1 @@
+Run AI-powered C# files with the power of Microsoft.Extensions.AI and Devlooped.Extensions.AI
