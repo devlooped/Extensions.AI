7a22fb9acc3211b55757fa63fbc672fcbe9929fe
Daniel Cazzulino <daniel@cazzulino.com>
2025-08-06 02:12:28 -0300

    Leverage overload resolution attribute to direct compiler
    We want our overload to take precedence over the built-in MEAI to provider smarter casting for your clients.

--- src/AI.CodeAnalysis/ChatClientExtensions.cs
+++ src/AI.CodeAnalysis/ChatClientExtensions.cs
@@ -7,6 +7,9 @@ using OpenAI.Chat;
 /// </summary>
 static class ChatClientExtensions
 {
+#if NET9_0_OR_GREATER
+    [System.Runtime.CompilerServices.OverloadResolutionPriorityAttribute(100)]
+#endif
     /// <summary>Gets an <see cref="IChatClient"/> for use with this <see cref="ChatClient"/>.</summary>
     public static IChatClient AsIChatClient(this ChatClient client) =>
         client as IChatClient ?? OpenAIClientExtensions.AsIChatClient(client);
