3db430ed100451c18532a2befdfc748f581ed1a1
Daniel Cazzulino <daniel@cazzulino.com>
2026-01-05 13:35:04 -0300

    Add helper factory methods to Chat
    Allows simpler construction of chat message like:
    
    ```csharp
    using static Devlooped.Extensions.AI.Chat;
    // ...
    var response = client.GetResponseAsync(
      [Assistant("how can I help?"), User("hello")])
    ```

---
+++ src/Tests/ChatExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Devlooped.Extensions.AI;
+using Microsoft.Extensions.AI;
+using static Devlooped.Extensions.AI.Chat;
+
+namespace Devlooped;
+
+public class ChatExtensionsTests
+{
+    [Fact]
+    public void FactoryMethods()
+    {
+        var message = User("hello");
+
+        Assert.Equal(ChatRole.User, message.Role);
+        Assert.Equal("hello", message.Text);
+
+        message = Assistant("hello");
+
+        Assert.Equal(ChatRole.Assistant, message.Role);
+        Assert.Equal("hello", message.Text);
+
+        // Can't use System without qualifying since it's also a namespace.
+        message = Chat.System("hello");
+        Assert.Equal(ChatRole.System, message.Role);
+        Assert.Equal("hello", message.Text);
+    }
+}
