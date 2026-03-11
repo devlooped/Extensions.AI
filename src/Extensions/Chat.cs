406eb7687d1f20439b7d402b3ef8fc04d714da7c
Daniel Cazzulino <daniel@cazzulino.com>
2026-03-11 18:10:36 -0300

    Improve debug rendering of Chat messages

--- src/Extensions/Chat.cs
+++ src/Extensions/Chat.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Diagnostics;
 using Microsoft.Extensions.AI;
 
 namespace Devlooped.Extensions.AI;
@@ -7,6 +8,8 @@ namespace Devlooped.Extensions.AI;
 /// Collection of <see cref="ChatMessage"/> for more convenient usage 
 /// in fluent construction of chat messages.
 /// </summary>
+[DebuggerTypeProxy(typeof(ChatDebugView))]
+[DebuggerDisplay("Count = {messages.Count}")]
 public class Chat : IEnumerable<ChatMessage>
 {
     readonly List<ChatMessage> messages = [];
@@ -60,4 +63,18 @@ public class Chat : IEnumerable<ChatMessage>
     IEnumerator<ChatMessage> IEnumerable<ChatMessage>.GetEnumerator() => messages.GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator() => messages.GetEnumerator();
+
+    sealed class ChatDebugView(Chat chat)
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        public ChatMessage[] Items
+        {
+            get
+            {
+                var items = new ChatMessage[chat.messages.Count];
+                chat.messages.CopyTo(items, 0);
+                return items;
+            }
+        }
+    }
 }
